b729257a80128b5e2654a8130784c04ccb8c8eaa
Yugogamer <yugogamer@gmail.com>
2020-06-05 15:32:42 +0200

    carteV1

---
+++ Vesion 1/CarteV1/src/sample/carte/BibliotequeImage.java
@@ -0,0 +1,14 @@
+package sample.carte;
+
+import javafx.scene.image.Image;
+
+public class BibliotequeImage {
+    public static Image contour = new Image("file:C:\\Users\\yugog\\Pictures\\waifu\\cadre.png");
+    public static Image plaine = new Image("file:E:\\Developpement\\git\\Projet-PTS2-advance-war-\\ressource\\GraphismeV1\\Plains.png");
+    public static Image ocean = new Image("file:E:\\Developpement\\git\\Projet-PTS2-advance-war-\\ressource\\GraphismeV1\\sea.png");
+    public static Image usine = new Image("file:E:\\Developpement\\git\\Projet-PTS2-advance-war-\\ressource\\GraphismeV1\\UsineNeutre.png");
+    public static Image ville = new Image("file:E:\\Developpement\\git\\Projet-PTS2-advance-war-\\ressource\\GraphismeV1\\VilleNeutreV1.png");
+
+    public static Image blueBazooka = new Image("file:E:\\Developpement\\git\\Projet-PTS2-advance-war-\\ressource\\GraphismeV1\\BlueBazookaV1.png");
+    public static Image redBazooka = new Image("file:E:\\Developpement\\git\\Projet-PTS2-advance-war-\\ressource\\GraphismeV1\\RedBazookaV1.png");
+}
---
+++ Vesion 1/CarteV1/src/sample/carte/Case.java
@@ -0,0 +1,136 @@
+package sample.carte;
+
+import javafx.event.EventHandler;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
+
+public class Case extends Canvas {
+    private Units uniter;
+    private Batiment building;
+    private Image terrain;
+    private GraphicsContext affichage;
+    private static final Image contours = new Image("file:E:\\Developpement\\git\\Projet-PTS2-advance-war-\\ressource\\GraphismeV1\\contourV1.png");
+    private static double largeur=32, hauteur=32;
+    private int zoom;
+
+    public Case(Image terrain, Batiment building, Units uniter) {
+        super(largeur, hauteur);
+        this.uniter = uniter;
+        this.building = building;
+        this.terrain = terrain;
+        init();
+    }
+
+    public Case(Image terrain, Batiment building) {
+        super(largeur, hauteur);
+        this.building = building;
+        this.terrain = terrain;
+        init();
+    }
+
+    public Case(Image terrain, Units units) {
+        super(largeur, hauteur);
+        this.uniter = units;
+        this.terrain = terrain;
+        init();
+    }
+
+    public Case(Image terrain) {
+        super(largeur, hauteur);
+        this.terrain = terrain;
+        init();
+    }
+
+
+    public void init()
+    {
+        zoom = 2;
+        affichage = this.getGraphicsContext2D();
+
+        this.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                affichage.drawImage(contours,0,0);
+            }
+        });
+        this.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                refreshAff();
+            }
+        });
+        this.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
+            @Override
+            public void handle(ContextMenuEvent event) {
+                ContextMenu menu = new ContextMenu();
+                System.out.println("je marche");
+                if(building != null)
+                {
+                    //Menu batimentMen = building.getMenu();
+                    MenuItem batiment = new MenuItem("batiment");
+                    menu.getItems().add(batiment);
+                }
+                if(uniter != null)
+                {
+                    //Menu Uniter = building.getMenu();
+                    MenuItem uniter = new MenuItem("uniter");
+                    menu.getItems().add(uniter);
+                }
+                menu.setAutoHide(true);
+                menu.show(Case.super.getParent(), event.getScreenX(), event.getScreenY());
+            }
+        });
+        refreshAff();
+        setScale();
+    }
+
+    public void zoom()
+    {
+        zoom++;
+        setScale();
+    }
+
+    public void dezoom()
+    {
+        zoom--;
+        setScale();
+    }
+
+
+
+
+    private void setScale()
+    {
+        if(zoom <= 0)
+        {
+            zoom = 1;
+        }else if(zoom >= 5)
+        {
+            zoom = 4;
+        }
+        super.setScaleX(zoom);
+        super.setScaleY(zoom);
+        super.setHeight(hauteur*zoom);
+        super.setWidth(hauteur*zoom);
+    }
+
+    public void refreshAff()
+    {
+        affichage.drawImage(terrain,0,0);
+        if(building != null)
+        {
+            affichage.drawImage(building.getImage(),0,0);
+        }
+        if(uniter != null)
+        {
+            affichage.drawImage(uniter.getImage(),0,0);
+        }
+    }
+}
