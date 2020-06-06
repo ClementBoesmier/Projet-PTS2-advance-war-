d2145db4169de82d3e9f0fe6204219607f046457
joladouille <cle.chec@laposte.net>
2020-06-06 15:12:09 +0200

    ajout systeme anti écraisement

--- Vesion 1/classe/classe AW2 V1/src/ClasseAdvencedWars/Case/Case.java
+++ Vesion 1/classe/classe AW2 V1/src/ClasseAdvencedWars/Case/Case.java
@@ -5,13 +5,14 @@ import ClasseAdvencedWars.units.Infantry;
 import ClasseAdvencedWars.units.RocketLauncher;
 import ClasseAdvencedWars.units.Tank;
 import ClasseAdvencedWars.units.Units;
+import Exception.FriendException;
 
 /**
  * 
  */
 public abstract class Case {
 
-    private Units unit;
+    private Units unit = null;
     
     private final Building building;
     
@@ -29,21 +30,27 @@ public abstract class Case {
      * @return
      */
     public Units getUnit() {
-        return unit;
+        return this.unit;
     }
 
-    public void setUnit(Units unit) {
-        this.unit = unit;
+    public void setUnit(Units unit) throws FriendException {
+       if(this.unit == null){
+           this.unit = unit;
+       }else if(this.unit.getOwner()== unit.getOwner()){
+           throw new FriendException("error : ally unit allredy in the case");
+       }else{
+           this.fight(unit);
+       }
     }
     
     /**
      * @return
      */
     public Building getBuilding() {
-        return building;
+        return this.building;
     }
     
-    public void fight(Units attack){
+    private void fight(Units attack){
         
         if(attack.getClass().equals(this.unit.getClass())){
             this.unit = null;
@@ -53,7 +60,7 @@ public abstract class Case {
                 this.unit = attack;
             }
             else{
-                //TODO : il meurt
+                attack = null;
             }
         }
         else if(attack instanceof RocketLauncher){
