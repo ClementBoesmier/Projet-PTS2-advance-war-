1871fed395305ba53f0d80b48f7f14f50083e6f1
joladouille <cle.chec@laposte.net>
2020-06-08 22:51:45 +0200

    ajout méthode walkable

--- Vesion 1/classe/classe AW2 V1/src/ClasseAdvencedWars/Case/Plain.java
+++ Vesion 1/classe/classe AW2 V1/src/ClasseAdvencedWars/Case/Plain.java
@@ -1,13 +1,18 @@
 package ClasseAdvencedWars.Case;
 
 import ClasseAdvencedWars.Case.Building.Building;
+import ClasseAdvencedWars.Team;
+import ClasseAdvencedWars.units.Infantry;
+import ClasseAdvencedWars.units.RocketLauncher;
+import ClasseAdvencedWars.units.Tank;
+import ClasseAdvencedWars.units.Units;
+import java.util.ArrayList;
 
 
 /**
  * 
  */
 public class Plain extends Case {
-
     /**
      * Default constructor
      */
@@ -18,5 +23,18 @@ public class Plain extends Case {
     
     public Plain(Building building) {
         super(building);
+    }
+
+    @Override
+    public boolean getWalkable(Units unit) {
+        boolean sortie = false;
+        if(
+        (unit instanceof Infantry) ||
+        (unit instanceof Tank) ||
+        (unit instanceof RocketLauncher)
+        ){
+            sortie = true;
+        }
+        return sortie;
     }
 }
