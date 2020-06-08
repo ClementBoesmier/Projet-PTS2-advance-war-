734894fc9aea6776a01ff165525bd2d805785673
joladouille <cle.chec@laposte.net>
2020-06-08 19:16:19 +0200

    create moveException

---
+++ Vesion 1/classe/classe AW2 V1/src/Exception/MoveException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exception;
+
+/**
+ *
+ * @author clement
+ */
+public class MoveException extends Exception{
+
+    public MoveException() {
+    }
+
+    public MoveException(String string) {
+        super(string);
+    }
+    
+}
