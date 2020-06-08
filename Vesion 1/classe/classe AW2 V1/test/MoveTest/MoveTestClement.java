4f24bc7bd7ec392831cb021e91be8c1119b1e859
joladouille <cle.chec@laposte.net>
2020-06-08 19:19:17 +0200

    création move Test WIP

---
+++ Vesion 1/classe/classe AW2 V1/test/MoveTest/MoveTestClement.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MoveTest;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ *
+ * @author clement
+ */
+public class MoveTestClement {
+    
+    
+    public MoveTestClement(){}
+    
+    @Before
+    public void startup(){
+        
+    }
+    
+    @After
+    public void TearDown(){
+        
+    }
+}
