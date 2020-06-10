8c372ae3348b67115c9121f1092d4cbf49789fb9
joladouille <cle.chec@laposte.net>
2020-06-10 19:48:44 +0200

    ajout test on end turn

--- Vesion 1/classe/classe AW2 V1/test/TestEndTurn/TestEndTurnClement.java
+++ Vesion 1/classe/classe AW2 V1/test/TestEndTurn/TestEndTurnClement.java
@@ -7,7 +7,14 @@ package TestEndTurn;
 
 import ClasseAdvencedWars.Case.Building.Base;
 import ClasseAdvencedWars.Case.Building.Town;
+import ClasseAdvencedWars.Case.Case;
+import ClasseAdvencedWars.Case.Ocean;
+import ClasseAdvencedWars.Case.Plain;
+import ClasseAdvencedWars.Maps;
 import ClasseAdvencedWars.Team;
+import ClasseAdvencedWars.units.Infantry;
+import Exception.FriendException;
+import Exception.MoveException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +31,11 @@ public class TestEndTurnClement {
     
     private Town town1;
     
+    private Infantry inf1;
+    
+    private Case[][] carte;
+    
+    private Maps map;
     
     public TestEndTurnClement(){}
     
@@ -33,14 +45,55 @@ public class TestEndTurnClement {
         this.base1 = new Base(team1);
         this.town1 = new Town();
         this.town1.setOwner(team1);
+        
+        this.inf1 = new Infantry(team1);
+        this.carte = new Case[3][3];
+        
+        for(int i = 0; i < 3; i++){
+            this.carte[i][0] = new Plain();
+        }
+        this.carte[0][1] = new Plain(this.town1);
+        this.carte[1][1] = new Plain(this.base1);
+        this.carte[2][1] = new Plain();
+        
+        this.carte[0][2] = new Plain();
+        this.carte[1][2] = new Ocean();
+        this.carte[2][2] = new Ocean();
+        
+        this.map = new Maps(3, 3, carte);
     }
     @After
     public void TearDown(){
         this.team1 = null;
         this.base1 = null;
         this.town1 = null;
+        this.inf1 = null;
+        this.carte = null;
+        this.map = null;
     }
-    
+    private void mapViewer(){
+        System.out.println("La carte :\n");
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(map.getCase(j, i) instanceof Plain){
+                    if(map.getCase(j, i).getUnit() instanceof Infantry){
+                        System.out.print("I");
+                    }else if(map.getCase(j, i).getBuilding() instanceof Town){
+                        System.out.print("T");
+                    }else if(map.getCase(j, i).getBuilding() instanceof Base){
+                        System.out.print("B");
+                    }else{
+                        System.out.print("P");
+                    }
+                }else if(map.getCase(j, i) instanceof Ocean){
+                    System.out.print("O");
+                }else{
+                    System.out.print("X");
+                }
+            }
+        System.out.println();
+        }
+    }
     @Test
     public void TestEndTurnBase(){
         int tempIncome = this.team1.getIncome();
@@ -54,7 +107,8 @@ public class TestEndTurnClement {
         this.town1.onEndTurn();
         assert(this.team1.getIncome() == tempIncome+7);
     }
-        @Test
+    
+    @Test
     public void TestEndTurnTeam(){
         int tempMoney = this.team1.getIncome();
         this.base1.onEndTurn();
@@ -62,5 +116,34 @@ public class TestEndTurnClement {
         this.base1.onEndTurn();
         this.team1.onEndTurn();
         assert(this.team1.getMoney()== tempMoney+20);
+    }
+    
+    @Test
+    public void TestEndTurnUnit() throws FriendException, MoveException{
+        this.mapViewer();
+        this.map.getCase(0, 0).setUnit(inf1);
+        this.map.getCase(0, 0).getUnit().moveStep(1, 0, map);
+        this.mapViewer();
+        this.map.getCase(1, 0).getUnit().moveStep(1, 0, map);
+        this.mapViewer();
+        this.map.getCase(2, 0).getUnit().moveStep(0, 1, map);
+        this.mapViewer();
+        this.map.getCase(2, 1).getUnit().moveStep(-1, 0, map);
+        this.mapViewer();
+        this.map.getCase(1, 1).getUnit().onEndTurn();
+        System.out.print("----FIN TOUR !!----");
+        this.mapViewer();
+        this.map.getCase(1, 1).getUnit().moveStep(1, 0, map);
+        this.mapViewer();
+        this.map.getCase(2, 1).getUnit().moveStep(0, -1, map);
+        this.mapViewer();
+        this.map.getCase(2, 0).getUnit().moveStep(-1, 0, map);
+        this.mapViewer();
+        this.map.getCase(1, 0).getUnit().moveStep(-1, 0, map);
+        this.mapViewer();
+    }
+    @Test
+    public void TestEndTurnGame(){
+        
     }
 }
