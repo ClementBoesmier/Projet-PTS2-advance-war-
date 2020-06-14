fb514c21aec43127dee2f6c7581ed1f837365cdd
Mathieu Lucas <mathieu.lucas@etudiant.univ-lr.fr>
2020-06-15 01:38:25 +0200

    DB

---
+++ Vesion 1/sqlite/connect/net/sqlitetutorial/TP5.1/src/tp5/pkg1/FXMLDocumentController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp5.pkg1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author mathi
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private AnchorPane pane;
+    @FXML
+    private Rectangle paddle;
+    
+    @FXML
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
